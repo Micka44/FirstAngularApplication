f3293f70d10e25cc831bf4d96009eff098bba700
Micka44 <mickaelmesnage@gmail.com>
2018-05-15 13:50:28 +0200

    add the display of all the new task entered

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -9,10 +9,21 @@ export class HomeComponent implements OnInit {
 
   itemCount: number = 4;
   btnText: string = 'Add an Item please';
+  goalText: string = '';
+  goals = [];
 
   constructor() { }
 
   ngOnInit() {
+    this.itemCount = this.goals.length;
+  }
+
+  addItem() {
+    if (this.goalText != '') {
+      this.goals.push(this.goalText);
+      this.goalText = '';
+      this.itemCount = this.goals.length;
+    }
   }
 
 }
